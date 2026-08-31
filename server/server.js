26f1b409c49c032ae3d52b51c76aa50ea6128b60
Anthers <anthersn@gmail.com>
2026-08-31 01:35:57 -0500

    Fix conexion socket.io en produccion

--- server/server.js
+++ server/server.js
@@ -7,7 +7,10 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: '*',
+    methods: ['GET', 'POST'],
+    credentials: false,
   },
+  transports: ['websocket', 'polling'],
 });
 
 const PORT = process.env.PORT || 3001;
